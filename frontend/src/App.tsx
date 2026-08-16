16f5ef26f3ab2fcab6306f59195d8c2858d702a0
sirironheart <127705750+Siriron@users.noreply.github.com>
2026-08-16 11:46:14 +0600

    Add files via upload

---
+++ frontend/src/App.tsx
@@ -0,0 +1,26 @@
+import { Routes, Route } from 'react-router-dom';
+import ErrorBoundary from './components/ErrorBoundary';
+import Layout from './components/Layout';
+import Home from './pages/Home';
+import RegisterSla from './pages/RegisterSla';
+import FileCheck from './pages/FileCheck';
+import Ledger from './pages/Ledger';
+import Docs from './pages/Docs';
+import NotFound from './pages/NotFound';
+
+export default function App() {
+  return (
+    <ErrorBoundary>
+      <Layout>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/register" element={<RegisterSla />} />
+          <Route path="/file" element={<FileCheck />} />
+          <Route path="/ledger" element={<Ledger />} />
+          <Route path="/docs" element={<Docs />} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </Layout>
+    </ErrorBoundary>
+  );
+}
