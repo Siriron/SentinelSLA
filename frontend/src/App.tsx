276450b4c6a0393d14b7f294637a7a6bdb8b6cb1
sirironheart <127705750+Siriron@users.noreply.github.com>
2026-08-19 21:15:53 +0600

    Add files via upload

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,6 +4,7 @@ import Layout from './components/Layout';
 import Home from './pages/Home';
 import RegisterSla from './pages/RegisterSla';
 import FileCheck from './pages/FileCheck';
+import CheckDetail from './pages/CheckDetail';
 import Ledger from './pages/Ledger';
 import Docs from './pages/Docs';
 import NotFound from './pages/NotFound';
@@ -16,6 +17,7 @@ export default function App() {
           <Route path="/" element={<Home />} />
           <Route path="/register" element={<RegisterSla />} />
           <Route path="/file" element={<FileCheck />} />
+          <Route path="/checks/:checkId" element={<CheckDetail />} />
           <Route path="/ledger" element={<Ledger />} />
           <Route path="/docs" element={<Docs />} />
           <Route path="*" element={<NotFound />} />
